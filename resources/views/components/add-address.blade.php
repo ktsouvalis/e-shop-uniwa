76245370b35d431b007c7e2353d6c6433a6cb8b5
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2025-01-24 18:16:25 +0200

    style address form

--- resources/views/components/add-address.blade.php
+++ resources/views/components/add-address.blade.php
@@ -1,20 +1,25 @@
-<form action="{{ route('address.store') }}" method="POST">
-    @csrf
-    <div >
-        <label for="name" >Ονοματεπώνυμο</label>
-        <input type="text" id="name" name="name" placeholder="Το όνομά σας" required>
-    </div>
-    <div >
-        <label for="address" >Οδός και Αριθμός</label>
-        <input type="text" id="address" name="address" placeholder="Αραχώβης 15" required>
-    </div>
-    <div >
-        <label for="city" >Πόλη</label>
-        <input type="text" id="city" name="city" placeholder="Αθήνα" required>
-    </div>
-    <div >
-        <label for="zip" >Τ.Κ.</label>
-        <input type="text" id="zip" name="zip" placeholder="12345" required>
-    </div>
-    <button type="submit">Add Address</button>
-</form>
+@push('links')
+    <link rel="stylesheet" href="{{ asset('css/forms.css') }}">
+@endpush
+<div class="action-container">
+    <form action="{{ route('address.store') }}" method="POST" class="action-form">
+        @csrf
+        <div>
+            <label for="name">Ονοματεπώνυμο</label>
+            <input type="text" id="name" name="name" placeholder="Το όνομά σας" required>
+        </div>
+        <div>
+            <label for="address">Οδός και Αριθμός</label>
+            <input type="text" id="address" name="address" placeholder="Αραχώβης 15" required>
+        </div>
+        <div>
+            <label for="city">Πόλη</label>
+            <input type="text" id="city" name="city" placeholder="Αθήνα" required>
+        </div>
+        <div>
+            <label for="zip">Τ.Κ.</label>
+            <input type="text" id="zip" name="zip" placeholder="12345" required>
+        </div>
+        <button type="submit">Add Address</button>
+    </form>
+</div>
