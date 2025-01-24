234d8657c5c2e3a1401aadd18292ac16a372c603
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2025-01-24 18:26:43 +0200

    fix cart-items style

--- resources/views/components/cart-item.blade.php
+++ resources/views/components/cart-item.blade.php
@@ -1,16 +1,5 @@
 @push('links')
-{{-- <style>
-    /* Remove spinners from number input fields */
-    input[type=number]::-webkit-outer-spin-button,
-    input[type=number]::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-
-    input[type=number] {
-        -moz-appearance: textfield;
-    }
-</style> --}}
+    <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
 @endpush
 <tr>
     <td>{{ $item->name }}</td>
@@ -19,14 +8,14 @@
     <td>{{ $item->price * $item->quantity }} €</td>
     
     @if(request()->routeIs('cart'))
-    <td  style="display: flex; justify-content: center; align-items: center;">
+    <td class="cart-item-container">
         <form action="{{route('cart.update_quantity', ['id'=>$item->id])}}" method="post">
             @csrf
-            <div >
+            <div>
                 <button type="button" onclick="this.nextElementSibling.stepDown()">-</button>
-                <input type="number" name="quantity" value="{{ $item->quantity }}" style="width: 50px;" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
+                <input type="number" name="quantity" value="{{ $item->quantity }}" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
                 <button type="button" onclick="this.previousElementSibling.stepUp()">+</button>
-                <button type="submit" class="bi bi-arrow-repeatσ" data-bs-toggle="tooltip" title="Ενημέρωση Ποσότητας"></button>
+                <button type="submit" class="bi bi-arrow-repeat" data-bs-toggle="tooltip" title="Ενημέρωση Ποσότητας"></button>
             </div>
         </form>
         <form action="{{ route('cart.remove_item', ['id' => $item->id]) }}" method="post">
