7518cbfca7761860d9afd792c3acd0b58924316d
Konstantinos Tsouvalis <ktsouvalis@uop.gr>
2025-02-05 09:02:43 +0200

    adds appropriate links to auth blades

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,6 +5,8 @@
             @csrf
             <div><input type="email" name="email" placeholder="Email" value="{{old('email')}}" required/></div>
             <div><button type="submit">Ανάκτηση Κωδικού</button></div>
+            <p class="action-footer"><a href="{{ route('register') }}">Δεν έχετε λογαριασμό; Εγγραφείτε</a></p>
+            <p class="action-footer"><a href="{{ route('login') }}">Έχετε λογαριασμό; Συνδεθείτε</a></p>
         </form>
     </div>
 </x-layout>
